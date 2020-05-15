ffe8daf29fe55dba92ff7b1b1bde0eb58386ec94
Paweł Marzec <pm@eoln.eu>
2020-05-15 11:48:50 +0200

    remove legacy scripts

--- docker-local/docker/wait-for/wait-for-kafka.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "** STARTUP - Checking for Broker connection..."
-
-source /opt/wait-for/wait-for.env
-
-sh /opt/wait-for/wait-for.sh $WAIT_FOR_DB_KAFKA_BROKER -t 240 -- echo "** STARTUP - Kafka connection successful!"

c79d37e26cf845eae8c270fd1ea33b6bbb6aa395
Lewis Daly <lewisdaly@me.com>
2020-06-17 16:52:38 +0800

    revert test/e2e/TestEnv.ts that somehow got changed in merge

--- test/e2e/TestEnv.ts
+++ test/e2e/TestEnv.ts
@@ -13,13 +13,16 @@ export type TestEnvType = {
     dfspa: string,
     dfspb: string,
     pisp: string,
+    pispContract: string,
     dfspaSchemeAdapter: string,
     dfspbSchemeAdapter: string,
     pispSchemeAdapter: string,
+    pispContractSchemeAdapter: string,
+    mlTestingToolkit: string,
   }
   currency: string,
   users: {
-    [index: string]: UserIdType, 
+    [index: string]: UserIdType,
   }
 }
 
@@ -29,10 +32,13 @@ const TestEnv: TestEnvType = {
   baseUrls: {
     dfspa: 'http://localhost:9003',
     dfspb: 'http://localhost:10003',
-    pisp:  'http://localhost:11003',
+    pisp: 'http://localhost:11003',
+    pispContract: 'http://localhost:12003',
     dfspaSchemeAdapter: 'http://localhost:5002',
-    dfspbSchemeAdapter:  'http://localhost:11003',
+    dfspbSchemeAdapter: 'http://localhost:6002',
     pispSchemeAdapter: 'http://localhost:7002',
+    pispContractSchemeAdapter: 'http://localhost:8002',
+    mlTestingToolkit: 'http://localhost:15000'
   },
   currency: 'USD',
   users: {
@@ -48,4 +54,3 @@ const TestEnv: TestEnvType = {
 }
 
 export default TestEnv;
-

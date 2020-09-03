83102deb7990381b98db392675b1c35da74710c6
Kevin Leyow <kleyow@gmail.com>
2020-09-02 22:49:02 -0500

    refactor: change to env variable

--- docker-local/postman/scripts/_05_seed_pisp.sh
+++ docker-local/postman/scripts/_05_seed_pisp.sh
@@ -3,9 +3,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PATH=$(cd ${DIR} && npm bin):$PATH
 POSTMAN_DIR="${DIR}/.."
+NEWMAN_REQUEST_DELAY=${NEWMAN_REQUEST_DELAY:=100}
 
 newman run \
-  --delay-request=100 \
+  --delay-request=${NEWMAN_REQUEST_DELAY} \
   --folder='pisp (data setup)' \
   --environment=${POSTMAN_DIR}/environments/Mojaloop-Local-Docker-Compose.postman_environment_PISP.json \
   ${POSTMAN_DIR}/OSS-New-Deployment-FSP-Setup-DFSPS.postman_collection.json
--- docker-local/postman/scripts/testE2ETransfers.sh
+++ docker-local/postman/scripts/testE2ETransfers.sh
@@ -3,8 +3,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PATH=$(cd ${DIR} && npm bin):$PATH
 POSTMAN_DIR="${DIR}/.."
+NEWMAN_REQUEST_DELAY=${NEWMAN_REQUEST_DELAY:=100}
 
 newman run \
-  --delay-request=100 \
+  --delay-request=${NEWMAN_REQUEST_DELAY} \
   --folder='E2E Tests' \
   ${POSTMAN_DIR}/PISP.postman_collection.json.postman_collection.json

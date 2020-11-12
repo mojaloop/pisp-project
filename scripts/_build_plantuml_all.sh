cdf103d806b1ad1b73f344efed6bc91d5b5e70f6
Lewis Daly <lewisdaly@me.com>
2020-11-12 13:53:30 +0800

    feat(ci/cd): work on all puml check

---
+++ scripts/_build_plantuml_all.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+for i in $(find ./docs -name '*.puml'); do
+  echo "found diagram: $i"
+  puml generate -p $i -o $(echo $i | sed 's/puml/png/g' | sed 's#docs#docs/out#g') -i $(echo $i | sed -e 's;[^/]*$;;');
+done

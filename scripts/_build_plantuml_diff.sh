42d9ca4593c0004dbb0bad87ed5052a58b49fda6
Kevin Leyow <kleyow@gmail.com>
2020-11-23 22:43:43 -0500

    chore: add script to generate puml diagrams (#98)
    * chore: add script to generate puml diagrams
    
    * test(hooks): working on pre-commit plantuml hook
    
    * feat(ci/cd): add a puml check
    
    * feat(ci/cd): work on all puml check
    
    * docs: update puml docs
    
    * docs: remove and rerender using new puml renderer
    
    * feat(docs): update puml docs
    
    * feat(docs): update puml docs
    
    * feat(docs): test commit
    
    * feat(docs): test commit
    
    * feat(docs): test commit
    
    * feat(docs): testing puml render hook
    
    * feat(ci/cd): fix test-docs step
    
    Co-authored-by: Lewis Daly <lewisdaly@me.com>

---
+++ scripts/_build_plantuml_diff.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+for i in $(git diff --staged --name-only `find ./docs -name '*.puml'`); do
+  echo "found diagram: $i"
+  # make the destination directory if not exists
+  mkdir -p $(dirname $i | sed 's#docs#docs/out#g')
+  puml generate -p $i -o $(echo $i | sed 's/puml/png/g' | sed 's#docs#docs/out#g') -i $(echo $i | sed -e 's;[^/]*$;;');
+done
+
+git add ./docs/out

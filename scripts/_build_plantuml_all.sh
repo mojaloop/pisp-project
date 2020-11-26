23c3f142df8a20f35e58472db227edd48db2bb85
Lewis Daly <lewisdaly@me.com>
2020-11-26 10:53:16 +0800

    feat(puml): test out svg render instead of png

--- scripts/_build_plantuml_all.sh
+++ scripts/_build_plantuml_all.sh
@@ -4,5 +4,5 @@ for i in $(find ./docs -name '*.puml'); do
   echo "found diagram: $i"
   # make the destination directory if not exists
   mkdir -p $(dirname $i | sed 's#docs#docs/out#g')
-  puml generate -p $i -o $(echo $i | sed 's/puml/png/g' | sed 's#docs#docs/out#g') -i $(echo $i | sed -e 's;[^/]*$;;');
+  puml generate -s $i -o $(echo $i | sed 's/puml/svg/g' | sed 's#docs#docs/out#g') -i $(echo $i | sed -e 's;[^/]*$;;');
 done

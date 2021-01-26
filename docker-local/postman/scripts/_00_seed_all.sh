8ae9c59bc678bdebf590a1022fe74759a0e5c111
Kevin Leyow <kleyow@gmail.com>
2021-01-26 15:48:04 -0500

    chore: add consent oracle to sandbox

--- docker-local/postman/scripts/_00_seed_all.sh
+++ docker-local/postman/scripts/_00_seed_all.sh
@@ -10,11 +10,3 @@ set -u
 
 ./_02_seed_oracle.sh
 ./_03_seed_dfspa.sh
-./_04_seed_dfspb.sh
-./_05_seed_pisp.sh
-./_06_seed_dfsp_simulator.sh
-./_07_seed_dfsp_a_msisdn.sh
-./_08_seed_dfsp_b_msisdn.sh
-./_09_seed_dfsp_simulator_msisdn.sh
-./_10_seed_pisp_msisdn.sh
-./_11_seed_dfsp_backend_parties.sh

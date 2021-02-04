d199e42371de5cc3fb1175989aeaf8bbb1be1a8b
Kevin Leyow <kleyow@gmail.com>
2021-02-04 10:25:51 -0500

    feat: add tests for oracle using ttk (#116)
    * feat: add tests for oracle using ttk
    
    * chore: fix circleci hosts
    
    * chore: fix scripts
    
    * chore: fix ci
    
    * chore: re-order responses
    
    * chore: add mongo back
    
    * chore: downgrade image
    
    Co-authored-by: Lewis Daly <lewis@vesselstech.com>

---
+++ docker-contract/docker/wait4/wait4.config.js
@@ -0,0 +1,83 @@
+module.exports = {
+  // format version sem-ver
+  // `v{major}.${minor}.${patch}`
+  wait4: 'v0.1.0',
+
+  // How many times should we retry waiting for a service?
+  retries: 60,
+
+  // How many ms to wait before retrying a service connection?
+  waitMs: 2500,
+
+  // services definitions
+  services: [
+    {
+      name: 'central-ledger',
+
+      // list of services to wait for
+      wait4: [
+        {
+          description: 'Kafka broker',
+          uri: 'kafka:29092',
+          method: 'ncat'
+        },
+        {
+          description: 'MySQL ledger',
+          uri: 'mysql:3306',
+          method: 'mysql',
+          // customized RC setup
+          rc: {
+            namespace: 'CLEDG',
+            configPath: '../config/default.json'
+          },
+          retries: 60
+        },
+        {
+          description: 'MongoDB object store',
+          uri: 'mongodb://objstore:27017/mlos',
+          method: 'mongo'
+        }
+      ]
+    },
+    {
+      name: 'account-lookup-service',
+      wait4: [
+        {
+          description: 'central-ledger api server',
+          uri: 'central-ledger:3001',
+          method: 'ncat',
+
+          // we have to wait much longer for central-ledger
+          // to spin up so we overload `retires` default parameter value
+          retries: 60
+        },
+        {
+          description: 'MySQL ALS',
+          uri: 'mysql-als:3306',
+          method: 'mysql',
+          // customized RC setup
+          rc: {
+            namespace: 'ALS',
+            configPath: '../config/default.json'
+          },
+          retries: 60
+        }
+      ]
+    },
+    {
+      name: 'als-consent-oracle',
+      wait4: [
+        {
+          description: 'MySQL Database',
+          uri: 'als-consent-oracle-mysql:3306',
+          method: 'mysqlAlt',
+          rc: {
+            namespace: 'CO',
+            configPath: '../config/development_db.json'
+          },
+          retries: 60
+        }
+      ]
+    }
+  ]
+}

706ac66400edab431935a867a1084458ff47b3c6
Lewis Daly <lewis@vesselstech.com>
2021-02-09 15:36:40 +1030

    feat: working on als consent oracle config

--- docker-live/_test_oracle.sh
+++ docker-live/_test_oracle.sh
@@ -2,16 +2,18 @@
 
 set -e
 
+ORACLE_ID_TYPE=ORACLE
+
 curl -s -X GET ${ELB_URL}/account-lookup-service/health
 curl -s -X GET ${ELB_URL}/account-lookup-service-admin/health
-
+echo ''
 
 # register the oracle
 curl -s -X POST ${ELB_URL}/account-lookup-service-admin/oracles \
   -H 'Content-Type: application/json' \
   -H 'Date: 2021-02-09T03:07:11.718Z' \
   -d '{
-      "oracleIdType": "CONSENT",
+      "oracleIdType": "'$ORACLE_ID_TYPE'",
       "endpoint": {
           "value": "als-consent-oracle/oracle",
           "endpointType": "URL"
@@ -20,31 +22,25 @@ curl -s -X POST ${ELB_URL}/account-lookup-service-admin/oracles \
       "isDefault": true
     }'
 
-exit 0
+echo ''
 
 # A super simple rough test of the Consents oracle
-curl -s -X POST ${ELB_URL}/account-lookup-service/CONSENTS/ \
-  --header 'Content-Type: application/json' \
-  --data-raw '[
-      {
-          "name": "scenario1",
-          "operation": "postTransfers",
-          "body": {
-      "from": {
-          "idType": "MSISDN",
-          "idValue": "123456789"
-      },
-      "to": {
-          "idType": "MSISDN",
-          "idValue": "987654321"
-      },
-      "amountType": "SEND",
-      "currency": "USD",
-      "amount":  "100",
-      "transactionType": "TRANSFER",
-      "note": "Test note",
-      "homeTransactionId": "123ABC"
-          }
-      }
-  ]' | jq
+# change ORACLE_ID_TYPE to MSISDN for a proper test, since
+# since the ALS doesn't like the `CONSENTS` type
+# TODO - fix in mojaloop/project#2015
+curl -s -X POST $ELB_URL/account-lookup-service/participants/$ORACLE_ID_TYPE/123 \
+  -H 'Accept: application/vnd.interoperability.participants+json;version=1' \
+  -H 'Content-Type: application/vnd.interoperability.participants+json;version=1.0' \
+  -H 'Date: 2021-02-09T03:07:11.718Z' \
+  -H 'FSPIOP-Source: dfspa' \
+  -d '{
+      "fspId": "dfspa"
+  }'
 
+curl -s -X GET $ELB_URL/account-lookup-service/participants/$ORACLE_ID_TYPE/123 \
+  -H 'Accept: application/vnd.interoperability.participants+json;version=1' \
+  -H 'Content-Type: application/vnd.interoperability.participants+json;version=1.0' \
+  -H 'Date: 2021-02-09T03:07:11.718Z' \
+  -H 'FSPIOP-Source: dfspa'
+
+echo 'Not monitoring endpoints here... go and check the logs yourself to check the callbacks'

9468403ab47a71a4c8b39777ae8858cf9b97e0bd
sridharvoruganti <36686863+sridharvoruganti@users.noreply.github.com>
2021-03-15 14:57:27 +0530

    test: e2e tests for linking accounts (#125)
    * test: e2e tests for linking accounts
    
    * chore: modify outbound accounts path

---
+++ test/e2e/PISP_Accounts.test.ts
@@ -0,0 +1,48 @@
+import TestEnv from './TestEnv'
+import axios from 'axios'
+
+describe('GET /accounts/{fspId}/{userId}', (): void => {
+  const expectedResp = {
+    accounts: [
+      {
+        accountNickname: 'dfspa.user.nickname1',
+        id: 'dfspa.username.1234',
+        currency: 'ZAR'
+      },
+      {
+        accountNickname: 'dfspa.user.nickname2',
+        id: 'dfspa.username.5678',
+        currency: 'USD'
+      }
+    ],
+    currentState: 'COMPLETED'
+  }
+
+  it('PISP requests DFSP to return user accounts for linking', async (): Promise<void> => {
+    const scenariosURI = `${TestEnv.baseUrls.pispThirdpartySchemeAdapterOutbound}/accounts/dfspa/username1234`
+    const response = await axios.get(scenariosURI)
+
+    // Assert
+    expect(response.status).toBe(200)
+    expect(response.data).toEqual(expectedResp)
+  })
+
+  it('PISP requests DFSP: Expect ID not found', async (): Promise<void> => {
+    const scenariosURI = `${TestEnv.baseUrls.pispThirdpartySchemeAdapterOutbound}/accounts/dfspa/test`
+    const idNotFoundResp = {
+      accounts: [],
+      errorInformation: {
+        errorCode: '3200',
+        errorDescription: 'Generic ID not found'
+      },
+      currentState: 'COMPLETED'
+    }
+
+    await axios.get(scenariosURI)
+      .catch(error => {
+        // Assert
+        expect(error.response.status).toBe(500)
+        expect(error.response.data).toEqual(expect.objectContaining(idNotFoundResp))
+      })
+  })
+})

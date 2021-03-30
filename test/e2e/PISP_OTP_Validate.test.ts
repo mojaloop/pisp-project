6b69a0d7c4edcf50db7580380953ceea0dee6ae2
Kevin Leyow <kleyow@gmail.com>
2021-03-30 14:01:39 -0500

    test: add otp validation tests

---
+++ test/e2e/PISP_OTP_Validate.test.ts
@@ -0,0 +1,78 @@
+/* eslint-disable import/no-named-as-default-member */
+import TestEnv from './TestEnv'
+import axios from 'axios'
+
+describe('/consentRequests/{ID}/validate: start->OTPIsValid', (): void => {
+  const consentRequestsId = 'c51ec534-ee48-4575-b6a9-ead2955b8069'
+
+  it('state should be OTPIsValid on valid OTP', async (): Promise<void> => {
+    // mojaloop-simulator is configured to return even authTokens as valid and
+    // odd as invalid
+    const validateRequest = {
+      authToken: '123456',
+      toParticipantId: 'dfspa'
+    }
+    const validateURI = `${TestEnv.baseUrls.pispThirdpartySchemeAdapterOutbound}/consentRequests/${consentRequestsId}/validate`
+    const validateResponse = await axios.patch(validateURI, validateRequest)
+    expect(validateResponse.status).toEqual(200)
+    expect(validateResponse.data.consent.consentRequestId).toEqual(consentRequestsId)
+    // these scope values are mocked for now in mojaloop-simulator
+    expect(validateResponse.data.consent.scopes).toEqual(
+      [
+        {
+          accountId: 'dfsp.blue.account.one',
+          actions: [
+            'accounts.getBalance',
+            'accounts.transfer'
+          ]
+        },
+        {
+          accountId: 'dfsp.blue.account.two',
+          actions: [
+            'accounts.getBalance',
+            'accounts.transfer'
+          ]
+        }
+      ]
+    )
+    expect(validateResponse.data.currentState).toEqual('OTPIsValid')
+  })
+})
+
+describe('/consentRequests/{ID}/validate: start->errored', (): void => {
+  const consentRequestsId = 'c51ec534-ee48-4575-b6a9-ead2955b8069'
+
+  it('state should be errored on invalid OTP', async (): Promise<void> => {
+    // mojaloop-simulator is configured to return even authTokens as valid and
+    // odd as invalid
+    const validateRequest = {
+      authToken: '123457',
+      toParticipantId: 'dfspa'
+    }
+    const validateURI = `${TestEnv.baseUrls.pispThirdpartySchemeAdapterOutbound}/consentRequests/${consentRequestsId}/validate`
+    const validateResponse = await axios.patch(validateURI, validateRequest)
+    expect(validateResponse.status).toEqual(200)
+    expect(validateResponse.data.consent.consentRequestId).toEqual(consentRequestsId)
+    // these scope values are mocked for now in mojaloop-simulator
+    expect(validateResponse.data.consent.errorInformation).toEqual({
+      scopes: [
+        {
+          accountId: 'dfsp.blue.account.one',
+          actions: [
+            'accounts.getBalance',
+            'accounts.transfer'
+          ]
+        },
+        {
+          accountId: 'dfsp.blue.account.two',
+          actions: [
+            'accounts.getBalance',
+            'accounts.transfer'
+          ]
+        }
+      ]
+    }
+    )
+    expect(validateResponse.data.currentState).toEqual('errored')
+  })
+})

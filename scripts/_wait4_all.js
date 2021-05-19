6fb151d327b2b0c2cdc4c39a23ebf21d596179bf
Kevin Leyow <kleyow@gmail.com>
2021-05-19 16:56:21 -0500

    chore: revert disabling and peg central ledger at v13.6.0

--- scripts/_wait4_all.js
+++ scripts/_wait4_all.js
@@ -10,7 +10,7 @@ const { execSync } = require('child_process')
 // Define the docker-compose containers you want to monitor here
 const expectedContainers = [
   'account-lookup-service',
-  // 'central-ledger', disabled due to unknown broker issue causing it to go unhealthy. needs investigation.
+  'central-ledger',
   // 'central-settlement', no health check
   // 'dfspa-backend', no health check
   // 'dfspa-sdk-scheme-adapter', no health check
@@ -29,7 +29,7 @@ const expectedContainers = [
   'pisp-thirdparty-api-adapter',
   // 'pisp-redis', no health check
   'kafka',
-  // 'ml-api-adapter',
+  'ml-api-adapter',
   'mysql',
   'mysql-als',
   'objstore',

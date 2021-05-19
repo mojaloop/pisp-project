96ddf22620171fa6dab11199b588aa0402d4193d
Kevin Leyow <kleyow@gmail.com>
2021-05-19 09:03:54 -0500

    chore: add setup for provider e2e test

--- test/e2e/PISP_Prelinking.test.ts
+++ test/e2e/PISP_Prelinking.test.ts
@@ -3,8 +3,8 @@ import axios from 'axios'
 
 describe('GET /linking/providers', (): void => {
   const expectedResp = {
-    accounts: ['dfspA', 'dfspB'],
-    currentState: 'providersLookupSuccess'
+    currentState: 'providersLookupSuccess',
+    providers: ['dfspA', 'dfspB']
   }
 
   it('PISP requests Thirdparty service enabled DFSPs', async (): Promise<void> => {
@@ -15,4 +15,8 @@ describe('GET /linking/providers', (): void => {
     expect(response.status).toBe(200)
     expect(response.data).toEqual(expectedResp)
   })
+
+  // There's no arguments that can be used for conditions for rules to make the
+  // ml-testing-toolkit return an error with the GET /services/{ServiceType} request
+  it.todo('PISP requests Thirdparty service enabled DFSPs - error returned')
 })

0e20d3f91e5415ed3fc48e4a573b54390393befb
Lewis Daly <lewis@vesselstech.com>
2021-06-23 18:51:01 +0930

    feat: temporarily override 3p-scheme-adapter

--- scripts/_restart_reseed_docker_local.sh
+++ scripts/_restart_reseed_docker_local.sh
@@ -9,3 +9,7 @@ docker-compose rm -f
 docker-compose up -d
 npm run wait-4-docker
 npx ml-bootstrap@0.3.11 -c $DIR/../docker-local/ml-bootstrap-config.json5
+
+# configure the web simulator
+export CONSENT_REQUEST_ID=b51ec534-ee48-4575-b6a9-ead2955b8069
+$DIR/_configure_web_simulator.sh

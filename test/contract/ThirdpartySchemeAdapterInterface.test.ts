f2f26ca3d2763f5324b0228a0ea4d549e43df1e4
Lewis Daly <lewis@vesselstech.com>
2021-07-27 16:33:15 +0930

    feat: working on example responses

--- test/contract/ThirdpartySchemeAdapterInterface.test.ts
+++ test/contract/ThirdpartySchemeAdapterInterface.test.ts
@@ -9,16 +9,71 @@ import TestEnv from "../e2e/TestEnv"
 
 describe('Thirdparty Scheme Adapter Interface', () => {
   describe('health', () => {
+
+    // There seems to be a bug with the TTK for this one...
     it.todo('GET /health')
-    it.todo('GET /metrics')
+    it('GET /metrics', async () => {
+      // Arrange
+      const tprURI = `${TestEnv.baseUrls.mlTestingToolkit}/metrics`
+      const expected = 'string'
+
+      // Act
+      const result = await axios.get(tprURI, {})
+
+      // Assert
+      expect(result.status).toBe(200)
+      expect(result.data).toStrictEqual(expected)
+    })
   })
   
+  // Hmm what's the purpose of this endpoint? Maybe we can skip it for now
   describe('authorizations', () => {
-    it.todo('POST /authorizations')
+    it.skip('POST /authorizations', async () => {
+      // Arrange
+      const tprURI = `${TestEnv.baseUrls.mlTestingToolkit}/authorizations`
+      const body = {}
+      const expected = 'string'
+
+      // Act
+      const result = await axios.post(tprURI, body)
+
+      // Assert
+      expect(result.status).toBe(200)
+      expect(result.data).toStrictEqual(expected)
+    })
   })
 
   describe('thirdpartyRequests', () => {
-    it.todo('POST /thirdpartyTransaction/partyLookup')
+    it.only('POST /thirdpartyTransaction/partyLookup', async () => {
+      // Arrange
+      const tprURI = `${TestEnv.baseUrls.mlTestingToolkit}/thirdpartyTransaction/partyLookup`
+      const body = {
+        "payee": {
+          "partyIdType": "PERSONAL_ID",
+            "partyIdentifier": "16135551212",
+              "partySubIdOrType": "string",
+                "fspId": "string",
+                  "extensionList": {
+            "extension": [
+              {
+                "key": "string",
+                "value": "string"
+              }
+            ]
+          }
+        },
+        "transactionRequestId": "b51ec534-ee48-4575-b6a9-ead2955b8069"
+      }
+      const expected = 'string'
+
+      // Act
+      const result = await axios.post(tprURI, body)
+
+      // Assert
+      expect(result.status).toBe(200)
+      expect(result.data).toStrictEqual(expected)
+    })
+
     it.todo('POST /thirdpartyTransaction/{ID}/initiate')
     it.todo('POST /thirdpartyTransaction/{ID}/approve')
     it.todo('POST /thirdpartyRequests/transactions/{ID}/authorizations')
@@ -40,7 +95,6 @@ describe('Thirdparty Scheme Adapter Interface', () => {
       // Assert
       expect(result.status).toBe(200)
       expect(result.data).toStrictEqual(expected)
-      
     })
     it.todo(`POST /linking/request-consent`)
     it.todo(`PATCH /linking/request-consent/{ID}/authenticate`)

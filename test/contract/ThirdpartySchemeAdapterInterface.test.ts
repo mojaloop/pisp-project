1728f31567bdb0df97556a3a5229caefefc1e2ac
Lewis Daly <lewis@vesselstech.com>
2021-07-27 15:58:56 +0930

    feat: first test working for 3p scheme adapter interface

--- test/contract/ThirdpartySchemeAdapterInterface.test.ts
+++ test/contract/ThirdpartySchemeAdapterInterface.test.ts
@@ -3,9 +3,11 @@
 
 // Tests out the 3p API interface running in the ttk
 
+import axios from "axios"
+import TestEnv from "../e2e/TestEnv"
+
 
 describe('Thirdparty Scheme Adapter Interface', () => {
-
   describe('health', () => {
     it.todo('GET /health')
     it.todo('GET /metrics')
@@ -24,10 +26,24 @@ describe('Thirdparty Scheme Adapter Interface', () => {
 
   describe('linking', () => {
     it.todo(`GET /linking/providers`)
-    it.todo(`GET /linking/accounts/{fspId}/{ userId }`)
+    it(`GET /linking/accounts/{fspId}/{userId}`, async () => {
+      // Arrange
+      const tprURI = `${TestEnv.baseUrls.mlTestingToolkit}/linking/accounts/dfspa/username1234`
+      const expected = [
+        { "accountNickname": "dfspa.user.nickname1", "currency": "ZAR", "id": "dfspa.username.1234" },
+        { "accountNickname": "dfspa.user.nickname2", "currency": "USD", "id": "dfspa.username.5678" }
+      ]
+     
+      // Act
+      const result = await axios.get(tprURI, {})
+
+      // Assert
+      expect(result.status).toBe(200)
+      expect(result.data).toStrictEqual(expected)
+      
+    })
     it.todo(`POST /linking/request-consent`)
     it.todo(`PATCH /linking/request-consent/{ID}/authenticate`)
     it.todo(`POST /linking/request-consent/{ID}/pass-credential`)
   })
-
 })

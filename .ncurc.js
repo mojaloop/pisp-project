cef35d8f447ff3a30b30cfc3e60d825a3ea5af58
Kevin <kleyow@gmail.com>
2022-06-30 13:24:03 -0500

    chore: prune

--- .ncurc.js
+++ .ncurc.js
@@ -3,16 +3,9 @@ module.exports = {
     // Upgrading past husky@4 to involves a full config migration with no current and apparent benefit.
     // So we are just sticking to husky@4.x.x for the time being.
     'husky',
-    // Caution advised in upgrading redis-mock past 0.52.0. Investigation needed.
-    'redis-mock',
-    // Upgrading past redis@3 to the next major version introduces a lot of breaking changes.
-    'redis',
-    '@types/redis',
     // Upgrading past jest|ts-jest|@types/jest@26 introduces a lot of breaking changes to current tests.
     'jest',
     'ts-jest',
-    '@types/jest',
-    // Upgrading past commander@7 introduces a lot of breaking changes.
-    'commander'
+    '@types/jest'
   ]
 }

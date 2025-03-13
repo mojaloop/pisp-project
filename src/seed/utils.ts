4bcb26f7fcf38d48579db38a42c70201b1ea2462
Juan Correa <gibaros@users.noreply.github.com>
2025-03-12 20:03:23 -0400

    feat(headers): update file headers to Mojaloop Foundation format

--- src/seed/utils.ts
+++ src/seed/utils.ts
@@ -1,3 +1,10 @@
+/*****
+License
+--------------
+Copyright © 2020-2025 Mojaloop Foundation
+The Mojaloop files are made available by the Mojaloop Foundation under the Apache License, Version 2.0 (the "License")
+*****/
+
 import { Result, RunResult } from './runResult'
 
 /**

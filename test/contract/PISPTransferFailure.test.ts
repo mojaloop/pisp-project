4aa1cceaa7a51f21956cad37de73539429377141
Juan Correa <gibaros@users.noreply.github.com>
2025-03-12 21:17:11 -0400

    feat(headers): update file headers to Mojaloop Foundation format

--- test/contract/PISPTransferFailure.test.ts
+++ test/contract/PISPTransferFailure.test.ts
@@ -1,3 +1,10 @@
+/*****
+License
+--------------
+Copyright © 2020-2025 Mojaloop Foundation
+The Mojaloop files are made available by the Mojaloop Foundation under the Apache License, Version 2.0 (the "License")
+*****/
+
 import TestEnv from '../e2e/TestEnv'
 import axios from 'axios'
 import * as TestErrorData from './data/mockErrorData.json'

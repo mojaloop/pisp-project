362c7aa1ae5c7b4420ccca735229cf1cba05f8bc
Juan Correa <gibaros@users.noreply.github.com>
2025-03-12 21:16:00 -0400

    feat(headers): update file headers to Mojaloop Foundation format

--- scripts/_decodeKeyHandleId.js
+++ scripts/_decodeKeyHandleId.js
@@ -1,3 +1,10 @@
+/*****
+License
+--------------
+Copyright © 2020-2025 Mojaloop Foundation
+The Mojaloop files are made available by the Mojaloop Foundation under the Apache License, Version 2.0 (the "License")
+*****/
+
 #!/usr/bin/env node
 
 const cbor = require('cbor')
